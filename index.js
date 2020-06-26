fa16db600b19755fb50fa62c825cf8573a300d75
Chris Burge <topher.burge@gmail.com>
2020-06-26 14:57:39 -0700

    add get and post requests to app.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const taskController = require("./controllers/tasks");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,4 +13,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.get('/', taskController.getTasks);
+app.get('/', taskController.getAddTasks);
+app.post('/', taskController.postAddTask);
+
 app.listen(3000, () => console.log("Server Up and running"));
+
