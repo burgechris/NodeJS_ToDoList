df03e578c29b5dbd778758a84133c0f414d224c1
Chris Burge <topher.burge@gmail.com>
2020-06-26 13:17:26 -0700

    create project structure.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const path = require('path')
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/static", express.static("public"));
-
-app.get('/', (req, res) => {
-  res.render('todoList.ejs');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.listen(3000, () => console.log("Server Up and running"));
