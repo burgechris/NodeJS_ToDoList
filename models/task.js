46ec22181c8e4d44d8b07b0b5f402f1bdb720129
Chris Burge <topher.burge@gmail.com>
2020-06-26 15:18:16 -0700

    refactor read/write to file code.

--- models/task.js
+++ models/task.js
@@ -1,22 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+	path.dirname(process.mainModule.filename),
+	"data",
+	"todolist.json"
+);
+
+const getTasksFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+	});
+}
+
 module.exports = class Task {
 	constructor(t) {
 		this.title = t;
 	}
 
 	save() {
-		const p = path.join(
-			path.dirname(process.mainModule.filename),
-			"data",
-			"todolist.json"
-		);
-		fs.readFile(p, (err, fileContent) => {
-			let tasks = [];
-			if (!err) {
-				tasks = JSON.parse(fileContent);
-			}
+		getTasksFromFile(tasks => {
 			tasks.push(this);
 			fs.writeFile(p, JSON.stringify(tasks), (err) => {
 				console.log(err);
@@ -25,16 +32,6 @@ module.exports = class Task {
 	}
 
 	static fetchAll(cb) {
-		const p = path.join(
-			path.dirname(process.mainModule.filename),
-			"data",
-			"todolist.json"
-		);
-		fs.readFile(p, (err, fileContent) => {
-			if (err) {
-				cb([]);
-			}
-			cb(JSON.parse(fileContent));
-		});
+		getTasksFromFile(cb);
 	}
 };
